fd387dd061d4bbfd90222750b69a8a5ffea1212a
Sharif Ahmed <sharifducse@gmail.com>
2019-10-10 12:18:55 +0600

    Solved leetcode problem "Maximum Sub Array"

---
+++ src/main/java/prep/leetcode/MaximumSubarray.java
@@ -0,0 +1,26 @@
+package prep.leetcode;
+
+/**
+ * @author sharifahmed
+ * @since 2019-10-09
+ */
+public class MaximumSubarray {
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    private static int maxSubArray(int[] nums) {
+        int maxCurrent = nums[0];
+        int maxGlobal = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+            if (maxCurrent > maxGlobal) {
+                maxGlobal = maxCurrent;
+            }
+        }
+
+        return maxGlobal;
+    }
+}
